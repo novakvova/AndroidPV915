047f7119e15edde51d9df5bdcd2ac081958ee15b
novakvova <novakvova@gmail.com>
2022-03-25 19:20:45 +0200

    Add validation field

--- android/app/src/main/java/com/example/shop/MainActivity.java
+++ android/app/src/main/java/com/example/shop/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.example.shop.dto.LoginDTO;
 import com.example.shop.service.NetworkService;
 import com.example.shop.service.Post;
+import com.google.android.material.textfield.TextInputLayout;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,11 +19,17 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvInfo;
+    private TextInputLayout txtFieldEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvInfo=findViewById(R.id.tvInfo);
+        txtFieldEmail=findViewById(R.id.txtFieldEmail);
+    }
+
+    public void handleLogin(View view) {
+        txtFieldEmail.setError("Маємо проблему");
     }
 
     public void onClickRequest(View view) {
