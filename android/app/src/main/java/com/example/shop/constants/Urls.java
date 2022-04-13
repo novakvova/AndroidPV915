192bad043281a629ebbfab4e34e33a72c04d7ca8
novakvova <novakvova@gmail.com>
2022-04-13 20:49:25 +0300

    Upload images to server

--- android/app/src/main/java/com/example/shop/constants/Urls.java
+++ android/app/src/main/java/com/example/shop/constants/Urls.java
@@ -1,5 +1,6 @@
 package com.example.shop.constants;
 
 public class Urls {
-    public static final String BASE = "http://10.0.2.2:5066";
+    //public static final String BASE = "http://10.0.2.2:5066";
+    public static final String BASE = "https://pv915.novakvova.com";
 }
